5f3a0ad1cfb4f25f844a6b40cadf4672517d57c1
Gabriel <gabeguimaraes13@gmail.com>
2022-11-25 17:10:02 -0300

    Ajusta diretorio

--- src/server.js
+++ src/server.js
@@ -1,38 +1,36 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io')
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
-
-const app = express()
-const server = http.createServer(app)
-const io = new Server(server)
-const port = 3333
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const port = 3333;
 
 app.get("/", (req, res) => {
-  return res.sendFile(__dirname + '/view/index.html')
-})
+  return res.sendFile(__dirname + "/view/index/index.html");
+});
 
 app.get("/seller", (req, res) => {
-  return res.sendFile(__dirname + '/view/seller.html')
-})
+  return res.sendFile(__dirname + "/view/seller/seller.html");
+});
 
-io.on('connection', (socket) => {
-  console.log('user connect')
+io.on("connection", (socket) => {
+  console.log("user connect");
 
-  socket.on('seller data', (msg) => {
-    socket.broadcast.emit('seller data', msg)
-  })
+  socket.on("seller data", (msg) => {
+    socket.broadcast.emit("seller data", msg);
+  });
 
-  socket.on('disconnect', () => {
-    console.log("user disconnect")
-  })
-})
+  socket.on("disconnect", () => {
+    console.log("user disconnect");
+  });
+});
 
 app.get("/manager", (req, res) => {
-  return res.sendFile(__dirname + '/view/manager.html')
-})
-
+  return res.sendFile(__dirname + "/view/manager/manager.html");
+});
 
 server.listen(port, () => {
-  console.log(`Servidor está rodando na porta ${port}`)
-})
+  console.log(`Servidor está rodando na porta ${port}`);
+});
