99b0faa55fb2cd37ed4dc1149a90708a7d723c0a
joao marcos carvalho <joaomarcos@DESKTOP-3BGRHFS.localdomain>
2022-11-25 10:41:15 -0300

    feat: initial project

---
+++ src/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io')
+
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+const port = 3333
+
+app.get("/", (req, res) => {
+  return res.sendFile(__dirname + '/view/index.html')
+})
+
+app.get("/seller", (req, res) => {
+  return res.sendFile(__dirname + '/view/seller.html')
+})
+
+io.on('connection', (socket) => {
+  console.log('user connect')
+
+  socket.on('seller data', (msg) => {
+    socket.broadcast.emit('seller data', msg)
+  })
+
+  socket.on('disconnect', () => {
+    console.log("user disconnect")
+  })
+})
+
+app.get("/manager", (req, res) => {
+  return res.sendFile(__dirname + '/view/manager.html')
+})
+
+
+server.listen(port, () => {
+  console.log(`Servidor está rodando na porta ${port}`)
+})
